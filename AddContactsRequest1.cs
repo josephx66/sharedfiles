d7c3e468b62a9110bd858c5ce6a0b7b93e49f770
josephx66 <josephx66@gmail.com>
2017-07-28 10:06:45 -0700

    AddContactsRequest commit

---
+++ AddContactsRequest1.cs
@@ -0,0 +1,36 @@
+﻿using Kvite.Web.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kvite.Web.Models.Requests
+{
+    public class AddContactsRequest
+    {
+
+        [StringLength(128), Required]
+        public string FirstName { get; set; }
+
+        [StringLength(128)]
+        public string LastName { get; set; }
+
+        [Range(0, 99999999999)]
+        public long? MobilePhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Url, StringLength(1000)]
+        public string PhotoUrl { get; set; }
+
+        public string SourceId { get; set; }
+
+        public ContactSourceKinds? SourceType { get; set; }
+
+        public string ContactUserId { get; set; }
+
+        public string OwnerUserId { get; set; }
+    }
+}
